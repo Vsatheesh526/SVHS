35ed4b677a62f3e3054ec20a9f16812acadc0aa9
lingappagarivinod <lvinodkumar944@gmail.com>
2026-06-08 12:06:23 +0530

    removed sitemap plugin

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,7 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// export default defineConfig({
-//   plugins: [react()],
-//   server: { port: 5173 },
-// });
-
-
-
-// import { defineConfig } from 'vite'
-// import sitemap from 'vite-plugin-sitemap'
-
-// export default defineConfig({
-//   plugins: [
-//     sitemap({
-//       hostname: 'https://svhs-git-main-vsatheesh526s-projects.vercel.app'
-//     })
-//   ]
-// });
+export default defineConfig({
+  plugins: [react()],
+  server: { port: 5173 },
+});
